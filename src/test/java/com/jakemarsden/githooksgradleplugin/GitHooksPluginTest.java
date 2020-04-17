a950910c8d9bf9592f6db4bce901e525c691a361
Jake Marsden <jakemarsdenjm@gmail.com>
2020-04-17 13:21:17 +1200

    TEST: Check report still uploaded on test failure

--- src/test/java/com/jakemarsden/githooksgradleplugin/GitHooksPluginTest.java
+++ src/test/java/com/jakemarsden/githooksgradleplugin/GitHooksPluginTest.java
@@ -2,11 +2,18 @@ package com.jakemarsden.githooksgradleplugin;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
 
 class GitHooksPluginTest {
+
+  // FIXME: Remove
+  @Test
+  void failure() {
+    fail();
+  }
 
   @Test
   void pluginExistsUnderCorrectId() {
